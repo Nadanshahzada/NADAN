ffab2c084cd212fbf3b8265e5df6f1ab53e94125
Nadanshahzada <75941440+Nadanshahzada@users.noreply.github.com>
2020-12-13 20:50:07 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - NADAN - CREATION----•◈•
+|__\~~) •||•NADAN-SHAZADA---------------•◈•
+|__(-----\  •◈•-----CHARSI POWER GANG---•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor NADAN SHAHZADA 
+\033[1;96 MALA DA TORO JAMO YAAR RAWALAY
+\033[1;96mFacebook  https://www.facebook.com/nadan.shahzada.5757
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/NADAN/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \NADAN SHAHZADA
+\033[1;96m  MALA DA TORO JAMO YAAR 
+\033[1;96mFacebook  https://www.facebook.com/nadan.shahzada.5757
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/NADAN/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "nadan"
+CorrectPassword = "nadan"
 
 loop = 'true'
 while (loop == 'true'):
